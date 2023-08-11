81c150279daa3e2d06e95d4ab07775bd998fe198
Yokohama <dimensionalbleedthrough48@gmail.com>
2023-08-11 13:48:12 +0800

    Modified 5 files

--- python/wordsplit.py
+++ python/wordsplit.py
@@ -1,49 +1,35 @@
-import os
-from tkinter import Tk
+import re
 
-sample_text1 = "輩出、正統、浸透、粘液、中枢、蟲、見境、責める、菠薐草、怨霊、詮議、意思疎通、歯を食いしばる、増殖、胎内、擁護、贖罪、黒鍵、外道、良識、打ち砕く、唄、失態、表向き、変貌、拝火教、系譜、秘宝、拮抗、八つ当たり、受肉、足止め、孵る、排斥、溶岩"
+sample_text1 = "琴瑟相和、近親相姦、魑魅魍魎、百戦錬磨、躊躇逡巡、弩級戦艦、近親相姦、敬天愛人、作画崩壊、管弦楽団、疾風怒濤、棘皮動物、糶糴売買、戦災孤児、絶体絶命、慇懃無礼、七不思議、帝国主義、磊磊落落、体外離脱、閲覧注意、有耶無耶、航空母艦、阿鼻叫喚、天上天下、八紘一宇"
 
 # text = input("Enter Japanese words separated by \"、\": ")
-
 def word_split1(text):
     """returns a word search thing for anki. fuck you."""
     return "\"front:re:(" + text.replace("、", "|") + ")\""
 
-# doesn't work, utf-8 not supported, so it fucks up
-def word_split2(text: str):
-    """generates a word search thing for anki and puts it in your clipboard. fuck you."""
-    command = "echo | set /p nul=" + "\"front:re:(" + text.replace("、", "|") + ")\"" + "| clip"
-    os.system(command)
-    print("Added expression to clipboard.")
-    # text = "\"front:re:(" + text.replace("、", "|") + ")\""
+def word_split2(text):
+    """same as above, but without the anki stuff."""
+    word_list = text.split("、")
+    final = ""
+    for word in word_list:
+        if re.search("[々一-鿿㐀-䶿𠀀-𪛟𪜀-𫜹𫝀-𫠝𫠠-𬺡𬺰-𮯠𰀀-𱍊𱍐-𲎯０-９Ａ-ｚ]", word):
+            final += word + "[<-]"
+        else:
+            final += word + "|"
+    final = re.sub("\|$", "", final)
+    return "\"front:re:(" + final + ")\""
 
-# doesn't work
-def word_split3(text: str):
-    r = Tk()
-    r.withdraw()
-    r.clipboard_clear()
-    r.clipboard_append("\"front:re:(" + text.replace("、", "|") + ")\"")
-    r.update() # now it stays on the clipboard after the window is closed
-    r.selection_get(selection = "CLIPBOARD")
-    r.destroy()
-    print("Added expression to clipboard.")
-
-# doesn't work
-def word_split4(text: str):
-    win32clipboard.OpenClipboard()
-    win32clipboard.EmptyClipboard()
-    win32clipboard.SetClipboardText("\"front:re:(" + text.replace("、", "|") + ")\"")
-    win32clipboard.CloseClipboard()
-    print("Added expression to clipboard.")
+def word_split3(text):
+    # "alt-forms">[A-Za-z0-9ぁ-んァ-ン一-鿿㐀-䶿𠀀-𪛟𪜀-𫜹𫝀-𫠝𫠠-𬺡𬺰-𮯠𰀀-𱍊𱍐-𲎯０-９Ａ-ｚ、「」]*</span>
+    return "\"back:re:(.alt-forms.>|、)(" + text.replace("、", "|") + ")\""
 
 def word_count(text: str):
     return len(text.split("、"))
 
-print(word_split1(sample_text1), "\n", word_count(sample_text1))
-input("exit\n")
-# word_split1(sample_text1)
-# print(word_split2(sample_text1))
-
-""" def add_to_clipBoard(text):
-    command = 'echo | set /p nul=' + text.strip() + '| clip'
-    os.system(command) """
+text1 = word_split1(sample_text1)
+text2 = word_split2(sample_text1)
+text3 = word_split3(sample_text1)
+with open("python\output.txt", "w", encoding="utf-8") as out:
+    text = text2
+    out.writelines(f"regex:\n{text2[11:-2]}\nanki regex:\n{text2}\nalt-forms:\n{text3}\nword count: {word_count(sample_text1)}")
+    print("done!")
