f781af69dee465e2763f1085263e5f37d447905b
obfuscated-end-user <dimensionalbleedthrough48@gmail.com>
2023-08-12 17:28:02 +0800

    Modified 4 files

---
+++ python/nycthemeron.py
@@ -0,0 +1,32 @@
+# can u help me figur it out wat it says, ill give u leik $5
+xOHYs0xvBnOiCSdMCwz_____a09qasPMhGdzV,rkj8GjtjBxAoGsgYS8CtY0xvBnOiCSdMCwziA, dO7x8ldMVs5oXeLnR3zxdO7x8ldMVs5oXeLnR3zx,o7ItBGAiiPHy5Q5ewQES=open,ord,range,len
+mjlcxnDGsgYS8CPSPzLQ=[{"".join(["\u0041"]):"".join(["\u3048","\u3051","\uff26","\u30ce","\u30ab",]),"".join(["\u0042"]):"".join(["\u3042","\uff30","\u304f",]),"".join(["\u0043"]):"".join(["\uff39","\u672c","\u3068",]),"".join(["\u0044"]):"".join(["\uff37","\uff1c",]),"".join(["\u0045"]):"".join(["\uff2a","\u304d","\uff34","\u30db","\u3090","\u30a4",]),"".join(["\u0046"]):"".join(["\uff2e","\u306f","\u3072",]),"".join(["\u0047"]):"".join(["\uff18","\uff2b","\u307e",]),"".join(["\u0048"]):"".join(["\uff23","\uff2d",]),"".join(["\u0049"]):"".join(["\u3082","\u3086","\u306d","\u30a6","\uff36",]),"".join(["\u004a"]):"".join(["\uff13",]),"".join(["\u004b"]):"".join(["\uff32","\u307b",]),"".join(["\u004c"]):"".join(["\u30df","\u30b3","\uff28",]),"".join(["\u004d"]):"".join(["\u30cb","\uff19","\u30bb","\u3057",]),"".join(["\u004e"]):"".join(["\uff15","\uff35","\u3084",]),"".join(["\u004f"]):"".join(["\u30b9","\u30c8","\uff3a","\u30ef","\uff2c",]),"".join(["\u0050"]):"".join(["\uff38","\u3078","\u30c6","\u305d",]),"".join(["\u0051"]):"".join(["\uff16",]),"".join(["\u0052"]):"".join(["\u308f","\u306e","\uff14","\u307f",]),"".join(["\u0053"]):"".join(["\uff27","\u3066","\u534a","\u30e6",]),"".join(["\u0054"]):"".join(["\uff21","\uff24","\u30ad","\u306c",]),"".join(["\u0055"]):"".join(["\uff2f","\u30ca","\u4e2d","\u58eb",]),"".join(["\u0056"]):"".join(["\uff17",]),"".join(["\u0057"]):"".join(["\u30e8","\uff25",]),"".join(["\u0058"]):"".join(["\u3091",]),"".join(["\u0059"]):"".join(["\uff22","\u3059",]),"".join(["\u005a"]):"".join(["\u4e11",]),"".join(["\u0020"]):"".join(["\u30f2","\uff31","\u5c71","\uff29","\u5b50","\u4e0b","\u3062",]),}]
+from string import ascii_letters,digits
+ZsPAtyeIW=print
+mjlcxnDGsgYS8CPSPzLQ.append({Ps6txOHYsHD44QXW3EbP:SatY0xvBnOiCSdMCwziA for(SatY0xvBnOiCSdMCwziA),(Ps6txOHYsHD44QXW3EbP)in mjlcxnDGsgYS8CPSPzLQ[int("".join([u"\u0030",]))].items()})
+from random import choice as qFvex3UXWP7DZ0ownHzE
+a9RiFMlEcbPBblYjtMo74aIBM2hz9eA2PtTXrxBU=lambda SatY0xvBnOiCSdMCwziA:"".join([ascii_letters,digits])
+ZsPAtyeIW(a9RiFMlEcbPBblYjtMo74aIBM2hz9eA2PtTXrxBU("dgYAs2h0WqK7UdamSLlt"))
+def ixNUosJT4MaDv3iL3mO7(xlVvzvFsbnhnVIoIsdA5,olo6jqasPMhGdzV6bd6k):
+    MIbZsPAtyeIW7Cc3tALl=""
+    for(eIuqeKhztJC4bUIBqh7N)in( xlVvzvFsbnhnVIoIsdA5):
+        ikO84By2XoMrN2bNl0Ur=eIuqeKhztJC4bUIBqh7N.replace(eIuqeKhztJC4bUIBqh7N,qFvex3UXWP7DZ0ownHzE(olo6jqasPMhGdzV6bd6k[eIuqeKhztJC4bUIBqh7N.upper()]))
+        MIbZsPAtyeIW7Cc3tALl=MIbZsPAtyeIW7Cc3tALl+ikO84By2XoMrN2bNl0Ur
+    return MIbZsPAtyeIW7Cc3tALl
+import math as qNuqzqTCuSLQHH2JLYqK
+from string import ascii_letters,digits
+RCZ4psq2cqQnRm1bDOT5="".join(["\u0061","\u0062","\u0063","\u0064","\u0065","\u0066","\u0067","\u0068","\u0069","\u006a","\u006b","\u006c","\u006d","\u006e","\u006f","\u0070","\u0071","\u0072","\u0073","\u0074","\u0075","\u0076","\u0077","\u0078","\u0079","\u007a","\u0041","\u0042","\u0043","\u0044","\u0045","\u0046","\u0047","\u0048","\u0049","\u004a","\u004b","\u004c","\u004d","\u004e","\u004f","\u0050","\u0051","\u0052","\u0053","\u0054","\u0055","\u0056","\u0057","\u0058","\u0059","\u005a","\u0030","\u0031","\u0032","\u0033","\u0034","\u0035","\u0036","\u0037","\u0038","\u0039",])
+pHXewieybwwT87L6s7B7 = "".join(qFvex3UXWP7DZ0ownHzE(RCZ4psq2cqQnRm1bDOT5)for RdiwzkibP6I in dO7x8ldMVs5oXeLnR3zxdO7x8ldMVs5oXeLnR3zx(int(qNuqzqTCuSLQHH2JLYqK.pi)*5))
+ZsPAtyeIW("".join(r"\u{:04x}".format(rkj8GjtjBxAoGsgYS8CtY0xvBnOiCSdMCwziA(FUURdiwzkibP6I))for(FUURdiwzkibP6I)in a9RiFMlEcbPBblYjtMo74aIBM2hz9eA2PtTXrxBU("Ps6txOHYsHD44QXW3EbP")))
+def KTpyE022d9Ulo8pDKuWG(ypyMXtC75cLDWAuGwL2i,v8soHsKOQzolrF732fFE):
+    qpDgeB6CMvesIK753ZQE=""
+    for(tfIZfKRVldSsUPoTnC1m)in(ypyMXtC75cLDWAuGwL2i):
+        qpDgeB6CMvesIK753ZQE=qpDgeB6CMvesIK753ZQE+[(tsFUURdiwzkibP6I8VVJ)for(X2zHqpMMs73DmILTnyTu), tsFUURdiwzkibP6I8VVJ in v8soHsKOQzolrF732fFE.items()if(tfIZfKRVldSsUPoTnC1m)in(X2zHqpMMs73DmILTnyTu)][int("".join([u"\u0030",]))]
+    return qpDgeB6CMvesIK753ZQE
+ZsPAtyeIW("".join(qFvex3UXWP7DZ0ownHzE(RCZ4psq2cqQnRm1bDOT5)for(K6kuPSPw7HlLBY1oeQIB)in(dO7x8ldMVs5oXeLnR3zxdO7x8ldMVs5oXeLnR3zx(4*5))))
+bNsWE3HBUaKxyhzAGyDN=ixNUosJT4MaDv3iL3mO7("\u0041\u0042\u0043\u0044\u0045", mjlcxnDGsgYS8CPSPzLQ[o7ItBGAiiPHy5Q5ewQES("")])
+yqmKeJkfGF9NQURgI7KO=KTpyE022d9Ulo8pDKuWG(bNsWE3HBUaKxyhzAGyDN,mjlcxnDGsgYS8CPSPzLQ[int("".join([u"\u0031",]))])
+rkj8GjtjBxAoUHXfMSq1=KTpyE022d9Ulo8pDKuWG("".join(["\u304d","\u3084","\uff17","\u3059","\u30bb","\uff26","\uff32","\u30a4","\u534a","\u30ef","\uff35","\u3090","\u672c","\u30ef","\uff17","\u304d","\u30ad","\u30ef","\u3072","\u306e","\uff21","\uff2d","\u3082","\uff35","\u307e","\uff27","\u30ad","\uff2d","\u30db","\uff22","\u30e8","\uff36","\uff28","\uff28","\uff15","\uff2a","\uff17","\uff34","\u308f","\uff2d","\uff26","\uff17","\u30db","\u30a6","\uff25","\u30a6","\uff28","\u30b3","\uff37","\u3082","\uff2a","\u3072","\u306d","\uff14","\u534a","\uff21","\uff30","\u304d","\u3072","\uff2c","\uff14","\u30db","\u306d","\u672c","\u30ab","\uff35","\uff18","\uff34","\uff21","\u30ce","\u30b3","\u30b3","\uff24","\uff23","\uff34","\u30ad","\uff23","\u308f","\uff34","\uff2a","\u30ad","\uff23","\u3086","\uff15","\uff18","\u534a","\u3086","\u30e8","\u30ce","\uff35","\uff24","\u30ad","\uff3a","\uff2b","\uff34","\u306c","\uff36","\u3084","\uff19","\uff22","\uff28","\uff36","\uff2e","\u30a4","\u306f","\u4e2d","\uff39","\uff32","\uff26","\u3068","\u30ce","\uff1c","\u30a4","\u3057","\u3086","\u672c","\uff17","\u30ce","\uff28","\u3082","\uff1c","\uff26","\uff21","\uff36","\uff3a","\u3084","\uff15","\u30ef","\uff35","\uff2a","\u30db","\uff37","\u306f","\u30c8","\uff14","\u306c","\uff2d","\u3048","\u30ad","\u3091"],), mjlcxnDGsgYS8CPSPzLQ[int("".join([u"\u0031",]))])
+hZhbLoWOkyteRlrZvIVn, sHRnowBElVAF5A3vBV6L, FacS6yl3QY3VVtAYPeeG=f"{pHXewieybwwT87L6s7B7}\u002e\u0074\u0078\u0074","".join(["\u0075","\u0074","\u0066","\u002d","\u0038",]),"".join(["\u0077",])
+with xOHYs0xvBnOiCSdMCwz_____a09qasPMhGdzV(hZhbLoWOkyteRlrZvIVn,FacS6yl3QY3VVtAYPeeG,encoding=sHRnowBElVAF5A3vBV6L)as(Bw6G5B4uYt6PSLYvbjIG):
+    Bw6G5B4uYt6PSLYvbjIG.writelines(f"{bNsWE3HBUaKxyhzAGyDN}\n{yqmKeJkfGF9NQURgI7KO}\n{rkj8GjtjBxAoUHXfMSq1}")
