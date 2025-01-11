48671b28e776559442c4211821a5aef33e1e7a18
obfuscated-end-user <dimensionalbleedthrough48@gmail.com>
2025-01-11 16:41:22 +0800

    modified 4 files

--- python/wordsplit.py
+++ python/wordsplit.py
@@ -1,6 +1,9 @@
+import collections
 import re
 
 sample_text1 = "琴瑟相和、近親相姦、魑魅魍魎、百戦錬磨、躊躇逡巡、弩級戦艦、敬天愛人、作画崩壊、管弦楽団、疾風怒濤、棘皮動物、糶糴売買、戦災孤児、絶体絶命、慇懃無礼、七不思議、帝国主義、磊磊落落、体外離脱、閲覧注意、有耶無耶、航空母艦、阿鼻叫喚、天上天下、八紘一宇、自己矛盾、花鳥風月、跳梁跋扈、阿諛追従、鰥寡孤独、曖昧模糊"
+kanji = "倪懼眄蟒臙閻侠龜沫驍牒瑞鍼灸俎鍮耽梁撞諮穂尉塑墾曹某殉窯艇帥迭卸畔丙輿膏鞠魯孟吻啄"
+dupes = "ああ、ああ、あい、あお、ぞう、漢字、漢字"
 
 # text=input("Enter Japanese words separated by \"、\": ")
 def word_split1(text):
@@ -23,7 +26,6 @@ def word_split3(text):
     # "alt-forms">[A-Za-z0-9ぁ-んァ-ン一-鿿㐀-䶿𠀀-𪛟𪜀-𫜹𫝀-𫠝𫠠-𬺡𬺰-𮯠𰀀-𱍊𱍐-𲎯０-９Ａ-ｚ、「」]*</span>
     return "\"back:re:(.alt-forms.>|、)(" + text.replace("、", "|") + ")\""
 
-kanji="倪懼眄蟒臙閻侠龜沫驍牒瑞鍼灸俎鍮耽梁撞諮穂尉塑墾曹某殉窯艇帥迭卸畔丙輿膏鞠魯孟吻啄"
 def kanji_separator(text):
     result = ""
     char_list = list(text)
@@ -35,10 +37,19 @@ def kanji_separator(text):
 def word_count(text: str):
     return len(text.split("、"))
 
+def find_duplicates(text: str):
+    result = ""
+    duplicates = [item for item, count in collections.Counter(text.split("、")).items() if count > 1]
+    for term in duplicates:
+        result += term  + "、"
+    result = result[:-1]
+    return result
+
+
 text1=word_split1(sample_text1)
 text2=word_split2(sample_text1)
 text3=word_split3(sample_text1)
 with open("python\output.txt", "w", encoding="utf-8") as out:
     text=text2
-    out.writelines(f"regex:\n{text2[11:-2]}\nanki regex:\n{text2}\nalt-forms:\n{text3}\nword count: {word_count(sample_text1)}\nseparated kanji:\n{kanji_separator(kanji)}")
+    out.writelines(f"regex:\n{text2[11:-2]}\nanki regex:\n{text2}\nalt-forms:\n{text3}\nword count: {word_count(sample_text1)}\nseparated kanji:\n{kanji_separator(kanji)}\nduplicate words:\n{find_duplicates(dupes)}")
     print("done!")
